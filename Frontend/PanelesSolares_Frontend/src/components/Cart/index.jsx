7311d43a5e7e27e1d6a32d42959edb0c63ba2cb5
Juliandos <95juliandos@gmail.com>
2025-03-31 09:23:48 -0600

    listo carrito

--- Frontend/PanelesSolares_Frontend/src/components/Cart/index.jsx
+++ Frontend/PanelesSolares_Frontend/src/components/Cart/index.jsx
@@ -1,18 +1,12 @@
 import PropTypes from "prop-types";
 import { useCart } from "../../components/Contexts/UseCart"
-// import CartItem from "../CartPage/CartItem";
+
+const apiUrl = import.meta.env.VITE_API_URL
 
 export default function Cart({ className, type }) {
 
-  const { cartItems } = useCart();
-
-  // if (cartItems.length === 0) {
-  //   return (
-  //     <div className={`w-full ${className || ""}`}>
-  //       <p>Your cart is empty</p>
-  //     </div>
-  //   );
-  // }
+  const { cartItems, removeFromCart } = useCart();
+  console.log(cartItems);
 
   return (
     <>
@@ -30,38 +24,36 @@ export default function Cart({ className, type }) {
                   <div className="flex space-x-[6px] justify-center items-center px-4 my-[20px]">
                     <div className="w-[65px] h-full">
                       <img
-                        src={`${
-                          import.meta.env.VITE_PUBLIC_URL
-                        }/assets/images/product-img-1.jpg`}
+                        src={`${apiUrl}/imagenes/imagen/${product.image}`}
                         alt=""
                         className="w-full h-full object-contain"
                       />
                     </div>
                     <div className="flex-1 h-full flex flex-col justify-center ">
                       <p className="title mb-2 text-[13px] font-600 text-qblack leading-4 line-clamp-2 hover:text-blue-600">
-                        iPhone 12 Pro Max 128GB Golen colour
+                        {product.title}
                       </p>
   
                       <p className="price">
                         <span className="offer-price text-qred font-600 text-[15px] ml-2">
-                          $38
+                          {product.offer_price}
                         </span>
                       </p>
                     </div>
-                  </div>
-                  <span className="mt-[20px] mr-[15px] inline-flex cursor-pointer">
-                    <svg
-                      width="8"
-                      height="8"
-                      viewBox="0 0 8 8"
-                      fill="none"
-                      className="inline fill-current text-[#AAAAAA] hover:text-qred"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path d="M7.76 0.24C7.44 -0.08 6.96 -0.08 6.64 0.24L4 2.88L1.36 0.24C1.04 -0.08 0.56 -0.08 0.24 0.24C-0.08 0.56 -0.08 1.04 0.24 1.36L2.88 4L0.24 6.64C-0.08 6.96 -0.08 7.44 0.24 7.76C0.56 8.08 1.04 8.08 1.36 7.76L4 5.12L6.64 7.76C6.96 8.08 7.44 8.08 7.76 7.76C8.08 7.44 8.08 6.96 7.76 6.64L5.12 4L7.76 1.36C8.08 1.04 8.08 0.56 7.76 0.24Z" />
-                    </svg>
-                  </span>
-                </li>
+                    </div>
+                    <span className="mt-[20px] mr-[15px] inline-flex cursor-pointer" onClick={() => removeFromCart(product.id)}>
+                      <svg
+                        width="8"
+                        height="8"
+                        viewBox="0 0 8 8"
+                        fill="none"
+                        className="inline fill-current text-[#AAAAAA] hover:text-qred"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M7.76 0.24C7.44 -0.08 6.96 -0.08 6.64 0.24L4 2.88L1.36 0.24C1.04 -0.08 0.56 -0.08 0.24 0.24C-0.08 0.56 -0.08 1.04 0.24 1.36L2.88 4L0.24 6.64C-0.08 6.96 -0.08 7.44 0.24 7.76C0.56 8.08 1.04 8.08 1.36 7.76L4 5.12L6.64 7.76C6.96 8.08 7.44 8.08 7.76 7.76C8.08 7.44 8.08 6.96 7.76 6.64L5.12 4L7.76 1.36C8.08 1.04 8.08 0.56 7.76 0.24Z" />
+                      </svg>
+                    </span>
+                  </li>
                 ))}
               
             </ul>
